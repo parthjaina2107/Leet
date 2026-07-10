6e881ca495772f559e4afef2fb9e10cf00436181
Parth Jaina <parthjaina.2107@gmail.com>
2026-07-10 19:35:29 +0530

    Time: 11 ms (13.67%), Space: 23.9 MB (10.55%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(int x : nums){
+            mp[x]++;
+        }
+
+        for(auto it : mp){
+            if(it.second == 1) return it.first;
+        }
+
+        return -1;
+    }
+};
