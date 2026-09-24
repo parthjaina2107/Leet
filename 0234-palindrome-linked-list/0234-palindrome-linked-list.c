3461b0a2efe2701363a7a11fc18574842e931fd8
Parth Jaina <parthjaina.2107@gmail.com>
2026-09-24 21:49:57 +0530

    Time: 4 ms (37.84%), Space: 45.4 MB (36.52%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.c
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -1,47 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
 struct ListNode* reverse(struct ListNode* head){
-    if(head == NULL || head->next == NULL){
+    if(head->next ==NULL){
         return head;
     }
-
     struct ListNode* curr = head;
-    struct ListNode* prev = NULL;
+    struct ListNode* prev= NULL;
 
-    while(curr != NULL){
+    while(curr!=NULL){
         struct ListNode* next = curr->next;
         curr->next = prev;
-        prev = curr;
-        curr = next;
+        prev=curr;
+        curr=next;  
     }
-
     return prev;
 }
-
 bool isPalindrome(struct ListNode* head) {
-
-    struct ListNode* slow = head;
-    struct ListNode* fast = head;
-
-    while(fast->next != NULL && fast->next->next != NULL){
-        fast = fast->next->next;
-        slow = slow->next;
-    }
-
-    struct ListNode* secondHalf = reverse(slow->next);
-
-    struct ListNode* first = head;
-    struct ListNode* second = secondHalf;
-
-    while(second != NULL){
-        if(first->val != second->val){
-            reverse(secondHalf);
+        struct ListNode* slow=head;
+        struct ListNode* fast=head;
+        fast=head;
+        if(head->next == NULL && head->val == 1){
+            return true;
+        }
+        if(head->next==NULL){
             return false;
         }
 
-        first = first->next;
-        second = second->next;
-    }
-
-    reverse(secondHalf);
-
-    return true;
+        while(fast->next !=NULL && fast->next->next !=NULL){
+            fast = fast->next->next;
+            slow=slow->next;
+        }
+        struct ListNode* newhead = reverse(slow->next);
+        struct ListNode* newnode= head;
+        while(newhead!=NULL){
+            if(newnode->val != newhead->val){
+                return false;
+            }
+            newnode=newnode->next;
+            newhead=newhead->next;
+        }
+        return true;
 }
